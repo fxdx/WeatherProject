90a12f27fa002a9104db6d9ba4db99dd827e850e
Paweł <pawel.dlugosz@multicom.krakow.pl>
2019-05-03 21:00:32 +0200

    adding forecast

--- main.py
+++ main.py
@@ -7,19 +7,41 @@ import json
 class City:
     def __init__(self, name, temperature, atmospheric_pressure):
         self.name = name
-        self.temperature = temperature
+        self.temperature = temperature.copy()
         self.atmospheric_pressure = atmospheric_pressure
 
 
-def sendingRequest(name):
-    api_key = "3946234ea602bd35b44f48feed6625c4"
+def FsendingRequest(name):
+    k = open('key.txt', 'r', encoding='utf-8-sig') #API key
+    api_key = k.read()
+    base_url = "http://api.openweathermap.org/data/2.5/forecast?"
+    city_name = name
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    resp = requests.get(complete_url)
+    return resp
+
+def FjsonInfo(resp, name):
+    response = resp.json()
+    if response['cod'] != '404':
+        print(response)
+    else:
+        print('city not found')
+
+def weatherForecast(name):
+    resp = FsendingRequest(name)
+    city_info = FjsonInfo(resp, name)
+
+
+def AsendingRequest(name): #requesting for data (actual weather)
+    k = open('key.txt', 'r', encoding='utf-8-sig') #API key
+    api_key = k.read()
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
     city_name = name
     complete_url = base_url + "appid=" + api_key + "&q=" + city_name
     resp = requests.get(complete_url)
     return resp
     
-def jsonInfo(resp, name):
+def AjsonInfo(resp, name): #transcripting informations (actual weather)
     response = resp.json()
     if response['cod'] != '404':
         main_json = response['main']
@@ -28,13 +50,15 @@ def jsonInfo(resp, name):
         city_info = City(name, temperature, atmospheric_pressure)
         return city_info
     else:
-        return
+        print('city not found')
 
 def actualWeather(name):
-    resp = sendingRequest(name)
-    city_info = jsonInfo(resp, name)
+    resp = AsendingRequest(name)
+    print(resp)
+    city_info = AjsonInfo(resp, name)
     print("{} - temperature: {}K ; atmospheric pressure: {}hPa".format(city_info.name, city_info.temperature, city_info.atmospheric_pressure))
 
 if __name__ == '__main__':
-    name = 'Krakow'
+    name = 'Krakow' #example
     actualWeather(name)
+    weatherForecast(name)
